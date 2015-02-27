e3f0626a1c6f1695c2bbc0bb5f0b3ea3647593e7
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2015-02-27 10:21:37 +0100

    Add Vagrant configuration

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+echo "========================="
+echo "Provisioning VM..."
+echo "========================="
+
+cd /home/vagrant
+
+echo "Installing mongodb..."
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+
+echo "Adding node.js repository..."
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+
+echo "Installing apps..."
+sudo apt-get install -y unzip bash nodejs git
+sudo npm install -g npm grunt-cli
+
+
+echo "Installing Yeoman & generator..."
+sudo npm install -g yo generator-meanjs mean-cli
+
+echo "========================="
+echo "Provisioning finished"
+echo "========================="
